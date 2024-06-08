395baff0597024f646191d1218c55810ace1f49e
sandu345 <sandunibatugedara11@gmail.com>
2024-06-08 10:53:12 +0530

    Login & Signup

---
+++ frontend/src/Signup.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Signup() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign Up</h2>
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your Name"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button className="btn btn-success w-100 rounded-0">Signup</button>
+          <p>You agree to our terms and policies.</p>
+          <Link to={"/"} className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+            Login
+          </Link>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
